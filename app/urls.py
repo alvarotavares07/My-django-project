fcf936c68124411a5bd4206bf87cea8c7fa9178f
alvarotavares07 <alvaro1@outlook.com>
2024-07-28 21:21:52 -0300

    Add README file and other updates

--- app/urls.py
+++ app/urls.py
@@ -2,11 +2,13 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
-from cars.views import cards_view  
+from cars.views import cards_view, new_car_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('cars/', cards_view),
+    path('cars/', cards_view, name='cars_list'),
+    path('new_car/', new_car_view, name='new_car')
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
 
 
